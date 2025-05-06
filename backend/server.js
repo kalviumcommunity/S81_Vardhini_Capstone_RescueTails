4f0d656475eccf7042694613d7e1dda2a174a0a7
minnu2204 <tadikondasrivardhini9@gmail.com>
2025-05-06 08:57:09 +0530

    done

--- backend/server.js
+++ backend/server.js
@@ -8,15 +8,17 @@ app.use(cors());
 app.use(express.json());
 
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(() => console.log('MongoDB connected'))
-  .catch(err => console.log(err));
+const connect=mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log('MongoDB connected'))
+.catch(err => console.log(err));
 
 
 const petRoutes = require('./routes/pets');
 app.use('/pets', petRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`)
+  connect
+})
+ 
