206d90ce82b946efe50a7084c60b7bc94406cc5a
minnu2204 <tadikondasrivardhini9@gmail.com>
2025-05-09 09:19:25 +0530

    database

---
+++ backend/models/user.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: /\S+@\S+\.\S+/,
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6,
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false,
+    },
+}, {
+    timestamps: true,
+});
+const User = mongoose.model('User', userSchema);
+module.exports = User;
