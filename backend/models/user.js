5dc28f417b8d5099ec5ed1edf066f635856c1a20
minnu2204 <tadikondasrivardhini9@gmail.com>
2025-05-14 10:47:02 +0530

    Implemented relationship

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,11 +1,11 @@
-import mongoose from 'mongoose'
+    import mongoose from 'mongoose'
 
-const userSchema = new mongoose.Schema({
-    username: String,
-    email: String,
-    password: String,
-    adoptedPets: [{type: mongoose.Schema.Types.ObjectId,ref:'Pet'}]
-})
+    const userSchema = new mongoose.Schema({
+        username: String,
+        email: String,
+        password: String,
+        adoptedPets: [{type: mongoose.Schema.Types.ObjectId,ref:'Pet'}]
+    })
 
-const User=mongoose.model('User',userSchema);
-export default User;
+    const User=mongoose.model('User',userSchema);
+    export default User;
